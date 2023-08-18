d28045764856ec38e80d3d908c0ff727b04bd471
JorgeDiezGracia <133242180+JorgeDiezGracia@users.noreply.github.com>
2023-08-18 07:48:12 +0200

    Update PruebasWeb.java
    Introduzco comentarios con cada paso ejecutado en los tests

--- ejercicioSelenium/src/test/java/tests/PruebasWeb.java
+++ ejercicioSelenium/src/test/java/tests/PruebasWeb.java
@@ -29,7 +29,7 @@ public class PruebasWeb {
         //Click en botón Parkside
         driver.findElement(By.className("ABrandOverview__Image")).click();
 
-        //Le mensaje de la class y lo guardo en un string, después comparo con assertequals el esperado y el valor del string
+        //Lee mensaje de la class y lo guardo en un string, después comparo con assertequals el esperado y el valor del string
         WebElement message = driver.findElement(By.className("result_count"));
         String value = message.getText();
         assertEquals("872 productos", value);
@@ -40,12 +40,14 @@ public class PruebasWeb {
         WebDriver driver = new ChromeDriver();
         driver.get("https://lidl.es");
         driver.findElement(By.cssSelector("button[class='cookie-alert-extended-button']")).click();
-        WebElement buscador = driver.findElement(By.name("query"));
 
+        // Busco elemento buscador e introduzco "salchichas", hago click
+        WebElement buscador = driver.findElement(By.name("query"));
         WebElement buscarboton = driver.findElement(By.className("primary"));
         buscador.sendKeys("salchichas");
         buscarboton.click();
 
+        // Comparo resultado que aparece en web con esperado
         WebElement message = driver.findElement(By.className("result_count"));
         String value = message.getText();
         assertEquals("2 productos", value);
@@ -56,19 +58,25 @@ public class PruebasWeb {
         WebDriver driver = new ChromeDriver();
         driver.get("https://lidl.es");
         driver.findElement(By.cssSelector("button[class='cookie-alert-extended-button']")).click();
-        WebElement buscador = driver.findElement(By.name("query"));
 
+        // Busco elemento buscador e introduzco "salchichas", hago click
+        WebElement buscador = driver.findElement(By.name("query"));
         WebElement buscarboton = driver.findElement(By.className("primary"));
         buscador.sendKeys("piscina perros");
         buscarboton.click();
 
+        // Busco carrito y hago click
         driver.findElement(By.className("frontpage-product-teaser__addtocart")).click();
-        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+        //Introduzco un wait para asegurar que se carga la web
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get("https://www.lidl.es/es/basket/validate");
+
+        // Navego hacia atrás
         driver.navigate().back();
         driver.get("https://www.lidl.es/es/basket/contents");
 
+        // Busco texto en web en el que aparezca "Tu cesta" y compruebo con esperado
         WebElement message = driver.findElement(By.className("nohero-heading"));
         String value = message.getText();
         assertEquals("Tu cesta", value);
